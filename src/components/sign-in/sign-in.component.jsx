575e8cafc6417028c55b7afb87f927155f3f4dfb
iPopop <popop@ntymail.com>
2020-07-23 14:35:14 +0200

    first authentication with google firebase in popup method - tests ok with chrome & firefox - video 07-04

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -6,6 +6,8 @@ import FormInput from "../form-input/form-input.component";
 
 import CustomButton from "../custom-button/custom-button.component";
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 class SignIn extends React.Component {
   constructor() {
     super();
@@ -51,7 +53,11 @@ class SignIn extends React.Component {
             label='password'
             required
           />
-          <CustomButton type='submit'> SIGN IN </CustomButton>
+          <CustomButton type='submit'> Sign in </CustomButton>
+          <CustomButton onClick={signInWithGoogle}>
+            {' '}
+            Sign in with Google{' '}
+          </CustomButton>
         </form>
       </div>
     );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,45 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDu3vP2Vd7wKWa3pxFHb8Z-belYmLsTQjw",
+  authDomain: "crwn-db-ee997.firebaseapp.com",
+  databaseURL: "https://crwn-db-ee997.firebaseio.com",
+  projectId: "crwn-db-ee997",
+  storageBucket: "crwn-db-ee997.appspot.com",
+  messagingSenderId: "145855830986",
+  appId: "1:145855830986:web:fb395d1b320a9dea1acd33",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+// auth.getRedirectResult().then(function(result) {
+//   if (result.credential) {
+//     // This gives you a Google Access Token. You can use it to access the Google API.
+//     let token = result.credential.accessToken;
+//     // ...
+//   }
+//   // The signed-in user info.
+//   let user = result.user;
+// }).catch(function(error) {
+//   // Handle Errors here.
+//   let errorCode = error.code;
+//   let errorMessage = error.message;
+//   // The email of the user's account used.
+//   let email = error.email;
+//   // The firebase.auth.AuthCredential type that was used.
+//   let credential = error.credential;
+//   // ...
+// });
+
+
+export default firebase;
