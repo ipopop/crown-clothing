071c95eae9230fa1e48cfa7d2e5c26991cf1c4a1
iPopop <popop@ntymail.com>
2020-07-25 20:31:14 +0200

    tunning styles in images on hover

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -10,23 +10,28 @@ const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
   return (
     <div className='collection-item'>
-      <div
-        className='image'
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-      />{" "}
+      <div className='image-container'>
+        <div
+          className='background-image'
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+          }}
+        />
+      </div>{" "}
       <div className='collection-footer'>
         <span className='name'> {name} </span>{" "}
         <span className='price'> {price} </span>{" "}
       </div>{" "}
-      <CustomButton onClick={() => addItem(item)} inverted> Add to cart </CustomButton>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        {" "}
+        Add to cart{" "}
+      </CustomButton>
     </div>
-  )
+  );
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  addItem: (item) => dispatch(addItem(item))
+  addItem: (item) => dispatch(addItem(item)),
 });
 
 export default connect(null, mapDispatchToProps)(CollectionItem);
