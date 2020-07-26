8ed6a2d6e6e8b2acccc93b48d43aabae40fc9ec4
iPopop <popop@ntymail.com>
2020-07-26 20:49:41 +0200

    removing redux-logger from prod build - video 14-05 - ok

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -39,7 +39,7 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className='test-warning'>
       *Please use the following test credit card for payments*
       <br />
-      4242 4242 4242 4242 - Exp: 12/20 - CVV: 321
+      4242 4242 4242 4242 - Exp: 12/20 - CVC: 321
     </div>
 
     <StripCheckoutButton price={total} />
